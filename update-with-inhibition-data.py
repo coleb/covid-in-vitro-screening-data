fe59a48046bc24243b9d48164ec8527b5f7a200c
Brian Cole <coleb@deshawresearch.com>
2020-04-15 12:18:47 -0400

    The update-with-inhibition-data.py script can be used to cross reference computational predictions back to this experimental data. Run it in the following way:
    $ python update-with-inhibition-data.py computational-predictions.csv 'My_Awesome_Score'
    
    This will use the SMILES columns in each of the CSV files to match the
    molecules against each other. This match is done in a chemically
    reasonable way using the InChI string which provides a useful default
    standardization scheme for normalizing tautomers and protonation
    states that are usually necessary for computational predictions, but
    irrelevant in the experimental data. If an exact InChI match isn't
    found, a fallback InChI search is performed using the non-stereo
    layers of the InChI to disregard stereochemistry differences as
    well. A lot of 3D prediction tools need stereochemistry defined, and
    this information is retained through the SMILES strings, however, the
    original experimental data may have been against a racemate. The tool
    will output a .png file of the scatter plot between the 'Inhibition
    index' and whatever column is specified on the commandline as the
    computational score.

---
+++ update-with-inhibition-data.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import sys
+import csv
+import pandas
+import requests
+import subprocess
+from rdkit import Chem
+
+def inchi_string(smiles):
+    smiles = smiles.replace('+H', 'H+') # annoying MOE variant RDKit doesn't like
+    return Chem.MolToInchi(Chem.MolFromSmiles(smiles))
+
+def inchi_isomer(smiles):
+    return '/'.join(inchi_string(smiles).split('/')[:4])
+
+IDX = -1
+def primary_key(row):
+    global IDX
+    IDX += 1
+    return IDX
+
+def best_match(smiles):
+    inchi = inchi_string(smiles)
+    found = invitro_data['INCHI'] == inchi
+    indices = list(found[found == True].index)
+    if not indices:
+        # fallback to fuzzy
+        fuzzy = inchi_isomer(smiles)
+        found = invitro_data['FUZZY'] == fuzzy
+        indices = list(found[found == True].index)
+
+    if not indices:
+        return -1
+
+    if len(indices) > 1:
+        print("Multiple matches found for", smiles)
+
+    return indices[0]
+
+invitro_data = pandas.read_csv('doi.org-10.1101-2020.04.03.023846-with-smiles.csv')
+invitro_data['INCHI'] = invitro_data['SMILES'].apply(inchi_string)
+invitro_data['FUZZY'] = invitro_data['SMILES'].apply(inchi_isomer)
+invitro_data['IDX'] = invitro_data.apply(primary_key, axis=1)
+
+data_to_merge = pandas.read_csv(sys.argv[1])
+data_to_merge['MERGE_IDX'] = data_to_merge['SMILES'].apply(best_match)
+
+merged = data_to_merge.merge(invitro_data, how='inner', left_on=['MERGE_IDX'], right_on=['IDX'])
+
+print('Found', len(merged), 'matches')
+
+from matplotlib import pyplot as plt
+plt.xlabel('Inhibition Index')
+plt.ylabel(sys.argv[2])
+plt.scatter(merged['Inhibition Index'], merged[sys.argv[2]])
+plt.savefig('Inhibition Index vs ' + sys.argv[2] + '.png')
+
